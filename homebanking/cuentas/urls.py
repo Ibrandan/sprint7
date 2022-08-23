beff8fb18d214a97c864c069989e942eae9d53a2
Ignacio Brandán <brandanignaciod@gmail.com>
2022-08-22 21:49:13 -0300

    login responsive

--- homebanking/cuentas/urls.py
+++ homebanking/cuentas/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'cuentas'
 urlpatterns = [
     path('', views.CuentaView.as_view(), name='cuenta'),
+    path('actividad/', views.ActividadView.as_view(), name='actividad'),
+    path('perfil/', views.PerfilView.as_view(), name='perfil'),
+    path('fondos/', views.FondosView.as_view(), name='fondos'),
+
 ]
--- homebanking/cuentas/views.py
+++ homebanking/cuentas/views.py
@@ -3,3 +3,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CuentaView(LoginRequiredMixin, TemplateView):
     template_name = 'cuentas/cuenta.html'
+
+class ActividadView(LoginRequiredMixin, TemplateView):
+    template_name = 'cuentas/actividad.html'
+
+class PerfilView(LoginRequiredMixin, TemplateView):
+    template_name = 'cuentas/perfil.html'
+
+class FondosView(LoginRequiredMixin, TemplateView):
+    template_name = 'cuentas/fondos.html'
+
+
+
