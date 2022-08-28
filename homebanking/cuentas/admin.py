1a09d6c9f948150e1c5db3159e30f0283688c860
IanDalton <105322605+IanDalton@users.noreply.github.com>
2022-08-28 19:03:18 -0300

    Base de datos y fix de login/create

--- homebanking/cuentas/admin.py
+++ homebanking/cuentas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Movimientos)
+admin.site.register(Cuenta)
+admin.site.register(Empleado)
--- homebanking/tarjetas/models.py
+++ homebanking/tarjetas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MarcasTarjeta(models.Model):
+    brand_id = models.TextField(primary_key=True, blank=True)  # This field type is a guess.
+    type_brand = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'marcas_tarjeta'
+
+
+
+class Tarjeta(models.Model):
+    card_id = models.AutoField(primary_key=True, blank=True)
+    card_number = models.TextField(unique=True, blank=True, null=True)
+    card_cvv = models.IntegerField()
+    card_gived = models.TextField(blank=True, null=True)
+    card_expiration = models.TextField(blank=True, null=True)
+    card_type = models.TextField(blank=True, null=True)
+    card_brand = models.ForeignKey('MarcasTarjeta', models.DO_NOTHING, db_column='card_brand', blank=True, null=True)
+    client = models.ForeignKey('cuentas.Cliente', models.DO_NOTHING, db_column='client', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Tarjeta'
+
